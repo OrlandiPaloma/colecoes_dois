7b221bf1c0b7b1d67973b64a724deb671bb037a3
Ubiratan <palomaorlandiw@gmail.com>
2022-06-07 16:30:10 -0300

    colecoes

---
+++ colecoes.py
@@ -0,0 +1,9 @@
+#coleções(listas)
+'''
+Some os valores (exemplo com coleções) dados uma coleção de dados idade (15, 46, 75, 34, 23) e imprima na tela a soma destes valores
+'''
+idades = [15, 46, 75, 34, 23]
+valor_inicial = 0
+for idade in idades:
+  valor_inicial = valor_inicial + idade
+print(valor_inicial)
